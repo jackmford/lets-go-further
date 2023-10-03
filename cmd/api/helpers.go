ec6e9800416c186606befcf278b1730875b5596d
jackmford <jackmitchellfordyce@gmail.com>
2023-10-03 10:52:07 -0500

    getting back into this - 3.4

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,8 +9,10 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]any
+
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
